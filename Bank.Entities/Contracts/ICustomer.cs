34caee5d0b4a80c4957fc255e316426c71859355
prajal77 <prajal.basnet0@gmail.com>
2024-08-05 16:12:52 +0545

    CustomerEntityCreated

---
+++ Bank.Entities/Contracts/ICustomer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Entities.Contracts
+{
+    public interface ICustomer
+    {
+        //Globally Unique identifier : generate unique identity
+        Guid CustomerId { get; set; }
+        long CustomerCode { get; set; }
+        string Address { get; set; }
+        string Landmark { get; set; }
+        string City { get; set; }
+        string Country { get; set; }
+        string Mobile { get; set; }
+
+    }
+}
---
+++ Bank.Entities/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Bank.Entities.Contracts;
+
+
+namespace Bank.Entities
+{
+    public class Customer : ICustomer
+    {
+        private Guid _customerId;
+        private long _customerCode;
+        private string _customerName;
+        private string _address;
+        private string _landmark;
+        private string _city;
+        private string _country;
+        private string _mobile;
+
+        public Guid CustomerId { get => _customerId; set => _customerId = value; }
+        public long CustomerCode { get => _customerCode; set => _customerCode = value; }
+        public string CustomerName { get => _customerName; set => _customerName = value; }
+        public string Address { get => _address; set => _address = value; }
+        public string Landmark { get => _landmark; set => _landmark = value; }
+        public string City { get => _city; set => _city = value; }
+        public string Country { get => _country; set => _country = value; }
+        public string Mobile { get => _mobile; set => _mobile = value; }
+    }
+}
